d39bfbfe28988cf26a399a3aa5c7410f6dbd0b5e
DedicatedSoftwareDev <saintnewworld@gmail.com>
2024-02-22 21:31:27 -0800

    modify

--- src/utils/nft.ts
+++ src/utils/nft.ts
@@ -145,9 +145,6 @@ export default async function mintNFT (
   const mintnftinterfaceInstruction = await mintNFTS(wallet, price, new_token_id)
   instructions.push(mintnftinterfaceInstruction)
   
-  /**
-   * This function is for Transaction with Retry
-   */
   try {
     const { txid } = await sendTransactionWithRetry(
       connection,
